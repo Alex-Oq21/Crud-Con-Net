ce3b346a2fce5cfbf7e3480de0f1f74d997bcaf3
Alexis <omaroquendo221@gmail.com>
2021-06-13 17:20:39 -0500

    Modelo de tabla a operar

---
+++ Tarjetas/Models/TarjetaDeCredito.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarjetas.Models
+{
+    public class TarjetaDeCredito
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(50)")]
+        public string Titular { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(15)")]
+        public string NumeroTarjeta { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string FechaExpiracion { get; set; }
+
+        
+    }
+}
